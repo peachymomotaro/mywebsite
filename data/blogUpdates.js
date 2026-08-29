2df12f6aa98b58a6ba4f66101d448698d6b3a43c
Peter Curry <curry.peter@googlemail.com>
2026-08-29 01:48:35 +0100

    Use exact Dennett blog update wording

--- data/blogUpdates.js
+++ data/blogUpdates.js
@@ -6,11 +6,11 @@ export const blogUpdates = [
     updates: [
       {
         date: "2026-08-29",
-        note: `In Guy Standing's *Human Capital: The Tragedy of the Education Commons* (Pelican, 2026), I came across a striking passage from the late philosopher Daniel Dennett about one of the dangers of AI, large language models and what he called "counterfeit people": that they could destroy the trust on which our shared knowledge depends. Standing quotes Dennett telling the BBC:
+        note: `In *Human Capital: The Tragedy of the Education Commons* (Pelican, 2026), Guy Standing quotes Dennett telling the BBC:
 
 “If we turn this wonderful technology we have for knowledge into a weapon for disinformation, we are in deep trouble ... because we won't know what we know, and we won't know who to trust, and we won't know whether we're informed or misinformed. We may become either paranoid and hyper-sceptical, or just apathetic and unmoved. Both of those are very dangerous avenues. And they're upon us.”
 
-Dennett added that “AIs are likely to evolve to get themselves reproduced. And the ones that reproduce the best will be the ones that are the cleverest manipulators of us human interlocutors. The boring ones we will cast aside, and the ones that hold our attention we will spread. All this will happen without any intention at all. It will be natural selection of software.”`,
+Dennett added that “AIs are likely to evolve to get themselves reproduced. And the ones that reproduce the best will be the ones that are the cleverest manipulators of us human interlocutors. The boring ones we will cast aside, and the ones that hold our attention we will spread. All this will happen without any intention at all. It will be natural selection of software."`,
         sourceUrl: "https://books.google.com/books/about/Human_Capital.html?id=oc81EQAAQBAJ",
         sourceTitle: "Guy Standing, Human Capital: The Tragedy of the Education Commons (2026)"
       }
